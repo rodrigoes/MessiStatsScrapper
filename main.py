122cc6af225307e5b8404cb7ee2c4e4a752b1e6c
rodrigoesrig <jairrodrigo92@gmail.com>
2023-03-29 20:30:34 -0300

    Create FastAPI

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/goals")
+async def read_goal_data():
+    with open("goal_data.json", "r") as file:
+        data = json.load(file)
+    return data
+
+@app.get("/aggregated")
+async def read_aggregated_data():
+    with open("aggregated_data.json", "r") as file:
+        data = json.load(file)
+    return data
+
+@app.get("/games")
+async def read_game_data_expanded():
+    with open("game_data_expanded.json", "r") as file:
+        data = json.load(file)
+    return data
